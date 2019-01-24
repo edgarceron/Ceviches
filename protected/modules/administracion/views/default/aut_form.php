acd2a0d72ed9e3794e2d51b4947ed0925a4cef64
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2019-01-24 13:15:44 -0500

    Implementación MU

--- protected/modules/administracion/views/default/aut_form.php
+++ protected/modules/administracion/views/default/aut_form.php
@@ -17,7 +17,7 @@
 			crossDomain: true,
 			jsonp: true,
 			'async':false,
-			'url':'http://dev.api.mensajerosurbanos.com/oauth/token',
+			'url':'https://dev.api.mensajerosurbanos.com/oauth/token',
 			'data':{
 				'client_id':'5b3d1c49d5608_murbanos',
 				'client_secret':'4b51943fda751148483027219396c113c23755d2',
