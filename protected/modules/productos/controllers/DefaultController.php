aa60e6d0fdbb450c0da30d62e0ab7639d452d4bb
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2019-02-19 09:30:46 -0500

    Modulo de productos V1.1

--- protected/modules/productos/controllers/DefaultController.php
+++ protected/modules/productos/controllers/DefaultController.php
@@ -73,7 +73,7 @@ class DefaultController extends Controller
 		$criteria->compare('modulo', $modulo);
 		$criteria->compare('accion', $accion);
 		$permisos = PerfilContenido::model()->find($criteria);
-		if(count($permisos) == 1){
+		if($permisos != null){
 			return true;
 		}
 		return false;
