c87a19bb0da098bd8a658d34cb6b1551760d9344
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2019-03-20 09:30:26 -0500

    Correcciones Jquery

--- protected/modules/administracion/controllers/acciones/VerPedidoAction.php
+++ protected/modules/administracion/controllers/acciones/VerPedidoAction.php
@@ -62,7 +62,7 @@ class VerPedidoAction extends CAction {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $post); 
-		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', "access_token: $access_token"]); 
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ["access_token: $access_token", 'Content-Type: application/json']); 
 		
 		$result=curl_exec ($ch);
 		curl_close($ch);
