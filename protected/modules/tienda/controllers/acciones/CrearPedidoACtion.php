67a25d7c3e296a338a2c854ccfddfee3700704d0
edgarceron <edgar.ceron@correounivalle.edu.co>
2019-04-08 16:00:26 -0500

    Correción

--- protected/modules/tienda/controllers/acciones/CrearPedidoACtion.php
+++ protected/modules/tienda/controllers/acciones/CrearPedidoACtion.php
@@ -427,6 +427,7 @@ class CrearPedidoAction extends CAction
 			$fechahora[1] = implode(":", $hms);
 			$start_time = $fechahora[1];
 		}
+		
 		$parametros = [
 			"id_user" => intval(OpcionesTienda::model()->find('descripcion = "id_user_mu"')['valor']), // ID de usuario
 			"type_service"=> 4, //Tipo de servicio
@@ -474,6 +475,8 @@ class CrearPedidoAction extends CAction
 		$result=curl_exec ($ch);
 		curl_close($ch);
 		$obj = json_decode($result, true);
+		print_r($obj);
+		exit;
 		$status_code = $obj['status_code'];
 		if($status_code == 200){
 			$data = $obj['data'];
